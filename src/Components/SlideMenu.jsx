ffeb2e7ddfcf95bd3e3f4e2dc14b750420876041
TellemBigAlsentya <alexhoffman26@gmail.com>
2023-05-24 15:56:04 -0700

    phone resolution bug fix

--- src/Components/SlideMenu.jsx
+++ src/Components/SlideMenu.jsx
@@ -30,10 +30,12 @@ function SlideMenu() {
         <h2>ABOUT</h2>
         <hr style={{width: '70%'}}></hr>
         <p>Welcome to Alex Hoffman's Portfolio Site!</p>
+        <br/>
         <p>All of the visual contents of this website were hand-drawn as an opportunity to showcase my personal taste in design and to allow a playful approach to showcasing the FrontEnd work I've completed. Each shelved item represents a project in my journey towards teaching myself web development skills. Each item links to their respective GitHub repositories and have thematically appropriate hover animations.</p>
+        <br/>
         <hr style={{width: '70%'}}></hr>
-        <p>Please feel free to stay a while and I hope you enjoy your visit! </p>
-        <button onClick={closeModal}>Close</button>
+        <p style={{fontWeight: 'bold'}}>Please feel free to stay a while and I hope you enjoy your visit! </p>
+        <span onClick={closeModal} label='Close Modal'>X</span>
       </Modal>
     </>
   );
