a379858b4995a2276f1d9aa0760126fe2a49c712
TellemBigAlsentya <alexhoffman26@gmail.com>
2023-05-30 10:05:16 -0700

    modal work

--- src/Components/SlideMenu.jsx
+++ src/Components/SlideMenu.jsx
@@ -28,14 +28,16 @@ function SlideMenu() {
       </Menu>
       <Modal isOpen={isModalOpen} onRequestClose={closeModal} className="custom-modal" overlayClassName="custom-overlay">
         <h2>ABOUT</h2>
-        <hr style={{width: '70%'}}></hr>
-        <p>Welcome to Alex Hoffman's Portfolio Site!</p>
-        <br/>
-        <p>All of the visual contents of this website were hand-drawn as an opportunity to showcase my personal taste in design and to allow a playful approach to showcasing the FrontEnd work I've completed. Each shelved item represents a project in my journey towards teaching myself web development skills. Each item links to their respective GitHub repositories and have thematically appropriate hover animations.</p>
-        <br/>
-        <hr style={{width: '70%'}}></hr>
-        <p style={{fontWeight: 'bold'}}>Please feel free to stay a while and I hope you enjoy your visit! </p>
-        <span onClick={closeModal} label='Close Modal'>X</span>
+        <div id='modal-content'>
+          <hr style={{width: '70%'}}></hr>
+          <p>Welcome to Alex Hoffman's Portfolio Site!</p>
+          <br/>
+          <p>All of the visual contents of this website were hand-drawn as an opportunity to showcase my personal taste in design and to allow a playful approach to showcasing the FrontEnd work I've completed. Each shelved item represents a project in my journey towards teaching myself web development skills. Each item links to their respective GitHub repositories and have thematically appropriate hover animations.</p>
+          <br/>
+          <hr style={{width: '70%'}}></hr>
+          <p style={{fontWeight: 'bold'}}>Please feel free to stay a while and I hope you enjoy your visit! </p>
+          <span onClick={closeModal} label='Close Modal'>X</span>
+        </div>
       </Modal>
     </>
   );
